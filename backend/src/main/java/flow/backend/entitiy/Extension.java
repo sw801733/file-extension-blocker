d97b0d129a3c56f62308b125cd9ba5b45c09e5b7
sw801733 <sw801733@naver.com>
2024-05-12 00:57:54 +0900

    feat : Extensions 테이블에 따라 Extension 엔티티 구성

--- backend/src/main/java/flow/backend/entitiy/Extension.java
+++ backend/src/main/java/flow/backend/entitiy/Extension.java
@@ -1,5 +1,38 @@
 package flow.backend.entitiy;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Entity
+@Table(name = "extensions")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Extension {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "extension", nullable = false)
+    private String extension;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    @Column(name = "is_checked", nullable = false)
+    private boolean isChecked;
 
 }
