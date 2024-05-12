9d9c326b4a6341c95f91a969dd8898b749eaf18d
sw801733 <sw801733@naver.com>
2024-05-12 12:34:45 +0900

    feat : 고정 확장자 추가 Controller 구현

--- backend/src/main/java/flow/backend/controller/FixExtensionController.java
+++ backend/src/main/java/flow/backend/controller/FixExtensionController.java
@@ -1,10 +1,26 @@
 package flow.backend.controller;
 
+import flow.backend.dto.request.ExtensionRequest;
+import flow.backend.dto.response.ExtensionResponse;
+import flow.backend.service.FixExtensionService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/fix-extension")
+@RequiredArgsConstructor
 public class FixExtensionController {
 
+    private final FixExtensionService fixExtensionService;
+
+    @PostMapping("/")
+    public ResponseEntity<ExtensionResponse> enableFixExtension(@RequestBody ExtensionRequest request) {
+        ExtensionResponse response = fixExtensionService.enableFixExtension(request);
+        return ResponseEntity.ok(response);
+    }
 }
