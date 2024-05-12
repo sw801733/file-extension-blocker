3f0fa37152e9555081ce7cb0428d108bf1a0bbe0
sw801733 <sw801733@naver.com>
2024-05-12 16:53:32 +0900

    feat : 선택한 커스텀 확장자 모두 조회 로직 구현

--- backend/src/main/java/flow/backend/service/CustomExtensionService.java
+++ backend/src/main/java/flow/backend/service/CustomExtensionService.java
@@ -5,6 +5,8 @@ import flow.backend.dto.response.ExtensionResponse;
 import flow.backend.entitiy.Extension;
 import flow.backend.repository.ExtensionRepository;
 import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -52,5 +54,17 @@ public class CustomExtensionService {
             .result(extensionName + " 커스텀 확장자가 삭제 되었습니다.")
             .build();
 
+    }
+
+    public List<ExtensionResponse> findAllEnableCustomExtensions() {
+        List<Extension> extensions = extensionRepository.findByTypeAndIsCheckedTrue("Custom");
+
+        return extensions.stream().map(extension
+            -> ExtensionResponse.builder()
+            .type("Custom")
+            .extension(extension.getExtension())
+            .checked(extension.isChecked())
+            .build()
+        ).collect(Collectors.toList());
     }
 }
