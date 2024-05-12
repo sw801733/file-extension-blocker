6b9afa1c30f952a99379024ec0890484cd4dfa04
sw801733 <sw801733@naver.com>
2024-05-12 11:09:23 +0900

    feat : type 별 선택한 확장자 모두 조회 구현
    - findByTypeAndCheckedTrue 을 통한 구현
    - 선택한 고정 확장자 or 커스텀 확장자 모두 조회

--- backend/src/main/java/flow/backend/repository/ExtensionsRepository.java
+++ backend/src/main/java/flow/backend/repository/ExtensionsRepository.java
@@ -1,7 +1,13 @@
 package flow.backend.repository;
 
 import flow.backend.entitiy.Extensions;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExtensionsRepository extends JpaRepository<Extensions, Long>{
+    // 선택한 고정 확장자 or 커스텀 확장자 모두 조회
+    List<Extensions> findByTypeAndCheckedTrue(String type);
+
+
+
 }
