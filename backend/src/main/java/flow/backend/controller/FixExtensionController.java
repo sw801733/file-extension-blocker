ca5890e9a6a26aadb773a0af78678b88407b6256
sw801733 <sw801733@naver.com>
2024-05-12 12:26:18 +0900

    feat : 고정 확장자 API 응답을 위한 Controller 생성

---
+++ backend/src/main/java/flow/backend/controller/FixExtensionController.java
@@ -0,0 +1,10 @@
+package flow.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/fix-extension")
+public class FixExtensionController {
+
+}
