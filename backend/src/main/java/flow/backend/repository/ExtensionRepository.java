d3f280ce67fa53568bb4906b8996ffdd0d238d42
sw801733 <sw801733@naver.com>
2024-05-12 11:22:19 +0900

    refact : ExtensionRepository 파일 명 변경

--- backend/src/main/java/flow/backend/repository/ExtensionRepository.java
+++ backend/src/main/java/flow/backend/repository/ExtensionRepository.java
@@ -4,7 +4,7 @@ import flow.backend.entitiy.Extension;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExtensionsRepository extends JpaRepository<Extension, Long>{
+public interface ExtensionRepository extends JpaRepository<Extension, Long>{
     // 선택한 고정 확장자 or 커스텀 확장자 모두 조회
     List<Extension> findByTypeAndCheckedTrue(String type);
 
