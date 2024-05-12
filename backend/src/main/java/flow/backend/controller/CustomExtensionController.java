8b9367c833cee8c6f95ebc769b69d6461712bf56
sw801733 <sw801733@naver.com>
2024-05-12 16:57:54 +0900

    feat : 커스텀 확장자 API 매핑 Controller 구현
    - 커스텀 확장자 추가
    - 커스텀 확장자 삭제
    - 추가한 커스텀 확장자 모두 조회

--- backend/src/main/java/flow/backend/controller/CustomExtensionController.java
+++ backend/src/main/java/flow/backend/controller/CustomExtensionController.java
@@ -1,8 +1,16 @@
 package flow.backend.controller;
 
+import flow.backend.dto.request.ExtensionRequest;
+import flow.backend.dto.response.ExtensionResponse;
 import flow.backend.service.CustomExtensionService;
-import flow.backend.service.FixExtensionService;
+
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +20,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomExtensionController {
 
     private final CustomExtensionService customExtensionService;
+
+    @PostMapping("/")
+    public ResponseEntity<ExtensionResponse> addCustomExtension(@RequestBody ExtensionRequest request) {
+        ExtensionResponse response = customExtensionService.addCustomExtension(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/")
+    public ResponseEntity<ExtensionResponse> deleteCustomExtension(@RequestBody ExtensionRequest request) {
+        ExtensionResponse response = customExtensionService.deleteCustomExtension(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<ExtensionResponse>> findAllCustomExtension() {
+        List<ExtensionResponse> responses = customExtensionService.findAllEnableCustomExtensions();
+        return ResponseEntity.ok(responses);
+    }
 }
