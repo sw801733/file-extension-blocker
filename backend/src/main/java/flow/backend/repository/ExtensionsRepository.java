87172077263dc3cd919d3ce94adedb6a42f72781
sw801733 <sw801733@naver.com>
2024-05-12 11:16:53 +0900

    refact : Extension 엔티티 명 변경 Extensions -> Extension

--- backend/src/main/java/flow/backend/repository/ExtensionsRepository.java
+++ backend/src/main/java/flow/backend/repository/ExtensionsRepository.java
@@ -1,12 +1,12 @@
 package flow.backend.repository;
 
-import flow.backend.entitiy.Extensions;
+import flow.backend.entitiy.Extension;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExtensionsRepository extends JpaRepository<Extensions, Long>{
+public interface ExtensionsRepository extends JpaRepository<Extension, Long>{
     // 선택한 고정 확장자 or 커스텀 확장자 모두 조회
-    List<Extensions> findByTypeAndCheckedTrue(String type);
+    List<Extension> findByTypeAndCheckedTrue(String type);
 
 
 
