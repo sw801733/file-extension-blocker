a10519a087612007cc4d19eacdace3469f9de0b9
sw801733 <sw801733@naver.com>
2024-05-12 12:17:34 +0900

    feat : 고정 확장자 삭제 로직 구현 - 코드 가독성을 위해 메서드 명을 check 에서 enable 로 변경

--- backend/src/main/java/flow/backend/service/FixExtensionService.java
+++ backend/src/main/java/flow/backend/service/FixExtensionService.java
@@ -15,7 +15,7 @@ public class FixExtensionService {
 
     private final ExtensionRepository extensionRepository;
 
-    public ExtensionResponse checkFixExtension(ExtensionRequest extensionRequest) {
+    public ExtensionResponse enableFixExtension(ExtensionRequest extensionRequest) {
 
         String extensionName = extensionRequest.getExtension();
         String extensionType = extensionRequest.getType();
@@ -31,9 +31,30 @@ public class FixExtensionService {
         return ExtensionResponse.builder()
             .extension(extensionName)
             .type(extensionType)
-            .checked(isChecked)
-            .result(extensionName + " 고정 확장자가 추가 되었습니다.")
+            .checked(true)
+            .result(extensionName + " 고정 확장자가 활성화 되었습니다.")
             .build();
+    }
+
+    public ExtensionResponse disableFixExtension(ExtensionRequest extensionRequest) {
+        String extensionName = extensionRequest.getExtension();
+        String extensionType = extensionRequest.getType();
+        boolean isChecked = extensionRequest.isChecked();
+
+        Extension extension = extensionRepository.findByExtension(extensionName);
+
+        if (!isChecked) {
+            extension.setChecked(false);
+            extensionRepository.save(extension);
+        }
+
+        return ExtensionResponse.builder()
+            .extension(extensionName)
+            .type(extensionType)
+            .checked(false)
+            .result(extensionName + " 고정 확장자가 비활성화 되었습니다.")
+            .build();
+
     }
 
 }
