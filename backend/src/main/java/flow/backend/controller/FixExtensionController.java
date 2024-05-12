7487762655eccb1a915c314a3fbef96ab247b4e0
sw801733 <sw801733@naver.com>
2024-05-12 17:55:23 +0900

    fix : 고정 확장자 Controller 매핑 수정
    - PathVariable 로 매핑 및 리소스 활용

--- backend/src/main/java/flow/backend/controller/FixExtensionController.java
+++ backend/src/main/java/flow/backend/controller/FixExtensionController.java
@@ -8,10 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,8 +22,8 @@ public class FixExtensionController {
 
     // 메서드에 따른 Controller 구분
 
-    @PostMapping("/")
-    public ResponseEntity<ExtensionResponse> enableFixExtension(@RequestParam("extension") String extension) {
+    @PostMapping("/{extension}")
+    public ResponseEntity<ExtensionResponse> enableFixExtension(@PathVariable String extension) {
         ExtensionRequest extensionRequest = ExtensionRequest.builder()
             .extension(extension)
             .build();
@@ -32,8 +31,8 @@ public class FixExtensionController {
         return ResponseEntity.ok(response);
     }
 
-    @DeleteMapping ("/")
-    public ResponseEntity<ExtensionResponse> disableFixExtension(@RequestParam("extension") String extension) {
+    @DeleteMapping ("/{extension}")
+    public ResponseEntity<ExtensionResponse> disableFixExtension(@PathVariable String extension) {
         ExtensionRequest extensionRequest = ExtensionRequest.builder()
             .extension(extension)
             .build();
@@ -41,7 +40,7 @@ public class FixExtensionController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping  ("/")
+    @GetMapping
     public ResponseEntity<List<ExtensionResponse>> findAllFixExtension() {
         List<ExtensionResponse> responses = fixExtensionService.findAllEnableFixExtensions();
         return ResponseEntity.ok(responses);
