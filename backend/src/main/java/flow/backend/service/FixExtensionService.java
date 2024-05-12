e962c9340a12ea1bb9a9cbceb8804d39b00ed120
sw801733 <sw801733@naver.com>
2024-05-12 12:13:19 +0900

    feat : FixExtensionService 고정 확장자 추가 로직 구현

--- backend/src/main/java/flow/backend/service/FixExtensionService.java
+++ backend/src/main/java/flow/backend/service/FixExtensionService.java
@@ -1,8 +1,39 @@
 package flow.backend.service;
 
+import flow.backend.dto.request.ExtensionRequest;
+import flow.backend.dto.response.ExtensionResponse;
+import flow.backend.entitiy.Extension;
+import flow.backend.repository.ExtensionRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class FixExtensionService {
+
+    private final ExtensionRepository extensionRepository;
+
+    public ExtensionResponse checkFixExtension(ExtensionRequest extensionRequest) {
+
+        String extensionName = extensionRequest.getExtension();
+        String extensionType = extensionRequest.getType();
+        boolean isChecked = extensionRequest.isChecked();
+
+        Extension extension = extensionRepository.findByExtension(extensionName);
+
+        if (!isChecked) {
+            extension.setChecked(true);
+            extensionRepository.save(extension);
+        }
+
+        return ExtensionResponse.builder()
+            .extension(extensionName)
+            .type(extensionType)
+            .checked(isChecked)
+            .result(extensionName + " 고정 확장자가 추가 되었습니다.")
+            .build();
+    }
 
 }
