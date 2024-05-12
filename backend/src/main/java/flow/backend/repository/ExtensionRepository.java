4c2f6198ba4710226bfb9a14a16a347846d468fc
sw801733 <sw801733@naver.com>
2024-05-12 11:27:10 +0900

    feat : ExtensionRepository Repository 빈 등록

--- backend/src/main/java/flow/backend/repository/ExtensionRepository.java
+++ backend/src/main/java/flow/backend/repository/ExtensionRepository.java
@@ -3,7 +3,9 @@ package flow.backend.repository;
 import flow.backend.entitiy.Extension;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ExtensionRepository extends JpaRepository<Extension, Long>{
     // 선택한 고정 확장자 or 커스텀 확장자 모두 조회
     List<Extension> findByTypeAndCheckedTrue(String type);
