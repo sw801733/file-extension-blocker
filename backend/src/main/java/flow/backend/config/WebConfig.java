a5738891adba7a03f5bf8cc59a7c9dab31a1911b
sw801733 <sw801733@naver.com>
2024-05-12 17:54:10 +0900

    fix : CORS 허용 Origin 로컬호스트로 변경
    * 사용 시 에러 발생

--- backend/src/main/java/flow/backend/config/WebConfig.java
+++ backend/src/main/java/flow/backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOrigins("http://127.0.0.1:5500/")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("Authorization", "Content-Type")
             .exposedHeaders("Custom-Header")
