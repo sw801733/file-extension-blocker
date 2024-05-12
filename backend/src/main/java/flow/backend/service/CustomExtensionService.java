9e22dfa425d9f3916cb336acc91002d8ae27fb32
sw801733 <sw801733@naver.com>
2024-05-12 16:45:44 +0900

    feat : 커스텀 확장자 추가 로직 구현

--- backend/src/main/java/flow/backend/service/CustomExtensionService.java
+++ backend/src/main/java/flow/backend/service/CustomExtensionService.java
@@ -1,5 +1,8 @@
 package flow.backend.service;
 
+import flow.backend.dto.request.ExtensionRequest;
+import flow.backend.dto.response.ExtensionResponse;
+import flow.backend.entitiy.Extension;
 import flow.backend.repository.ExtensionRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -12,5 +15,22 @@ public class CustomExtensionService {
 
     private final ExtensionRepository extensionRepository;
 
+    public ExtensionResponse enableCustomExtension(ExtensionRequest extensionRequest) {
 
+        String extensionName = extensionRequest.getExtension();
+
+        Extension extension = Extension.builder()
+            .extension(extensionName)
+            .type("Custom")
+            .isChecked(true)
+            .build();
+        extensionRepository.save(extension);
+
+        return ExtensionResponse.builder()
+            .extension(extensionName)
+            .type("Custom")
+            .checked(true)
+            .result(extensionName + " 커스텀 확장자가 활성화 되었습니다.")
+            .build();
+    }
 }
