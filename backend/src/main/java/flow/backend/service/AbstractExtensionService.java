345f6a795473c650560b3de987cff9aaee3d0d6b
sw801733 <sw801733@naver.com>
2024-06-10 20:46:27 +0900

    refact : Service 추상 클래스 필드와 메서드 구현

--- backend/src/main/java/flow/backend/service/AbstractExtensionService.java
+++ backend/src/main/java/flow/backend/service/AbstractExtensionService.java
@@ -1,5 +1,29 @@
 package flow.backend.service;
 
+import flow.backend.dto.response.ExtensionResponse;
+import flow.backend.entitiy.Extension;
+import flow.backend.repository.ExtensionRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public abstract class AbstractExtensionService {
+
+    private final ExtensionRepository extensionRepository;
+
+    protected abstract String getExtensionType();
+
+    protected List<ExtensionResponse> findAllEnableCustomExtensions() {
+        List<Extension> extensions = extensionRepository.findByTypeAndIsCheckedTrue(getExtensionType());
+
+        return extensions.stream().map(extension
+            -> ExtensionResponse.builder()
+            .type(getExtensionType())
+            .extension(extension.getExtension())
+            .checked(extension.isChecked())
+            .build()
+        ).collect(Collectors.toList());
+    }
 
 }
