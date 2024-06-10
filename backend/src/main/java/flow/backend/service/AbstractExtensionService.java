2be3a9f392b73d3fe88c28cd1db7614631843fb5
sw801733 <sw801733@naver.com>
2024-06-10 22:19:11 +0900

    fix : 추상 클래스 어노테이션과 필드 메서드 범위 수정

--- backend/src/main/java/flow/backend/service/AbstractExtensionService.java
+++ backend/src/main/java/flow/backend/service/AbstractExtensionService.java
@@ -3,18 +3,22 @@ package flow.backend.service;
 import flow.backend.dto.response.ExtensionResponse;
 import flow.backend.entitiy.Extension;
 import flow.backend.repository.ExtensionRepository;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
 @RequiredArgsConstructor
 public abstract class AbstractExtensionService {
 
-    private final ExtensionRepository extensionRepository;
+    protected final ExtensionRepository extensionRepository;
 
     protected abstract String getExtensionType();
 
-    protected List<ExtensionResponse> findAllEnableCustomExtensions() {
+    public List<ExtensionResponse> findAllEnableCustomExtensions() {
         List<Extension> extensions = extensionRepository.findByTypeAndIsCheckedTrue(getExtensionType());
 
         return extensions.stream().map(extension
