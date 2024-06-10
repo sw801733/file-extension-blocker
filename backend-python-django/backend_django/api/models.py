1e8ff81619f53e4c3358eabe9ea2625d7b35d913
sw801733 <sw801733@naver.com>
2024-06-11 00:29:59 +0900

    feat : Extension Model 구현

--- backend-python-django/backend_django/api/models.py
+++ backend-python-django/backend_django/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Extension(models.Model):
+    id = models.AutoField(primary_key=True)
+    extension = models.CharField(null=False)
+    type = models.CharField(null=False)
+    is_Checked = models.BooleanField(null=False)
+
+    def __str__(self):
+        return self.extension
