cfa873ae7d82291d29b174c7d0791ac46b3d097d
sw801733 <sw801733@naver.com>
2024-07-05 22:27:02 +0900

    feat : fixExtensionController 추가

---
+++ backend-node-js/src/controllers/fixExtensionController.js
@@ -0,0 +1,18 @@
+
+
+export class fixExtensionController {
+
+    // 서비스 인스턴스 생성
+
+    getAllFixExtension = async (req, res, next) => {
+        // 모든 고정 확장자 조회
+    }
+
+    enableFixExtension = async (req, res, next) => {
+        // 고정 확장자 활성화
+    }
+
+    disableFixExtension = async (req, res, next) => {
+        // 고정 확장자 비활성화
+    }
+}
