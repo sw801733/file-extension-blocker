c38155fe431b8b3d4883ea1f0fdd2122287f14e5
sw801733 <sw801733@naver.com>
2024-07-09 22:27:04 +0900

    feat : fixExtensionService 생성
    - addExtension 기능 추가

---
+++ backend-node-js/src/services/fixExtensionService.js
@@ -0,0 +1,30 @@
+const Extensions = require("../models/extensions");
+
+export class fixExtensionService {
+  addExtension = async (req, res, next) => {
+    const extensionName = req.extensionName;
+    const extensionType = req.extensionType;
+
+    const extension = Extensions.findOne({
+      where: {
+        extension: extensionName,
+        type: extensionType,
+      },
+    });
+
+    const isChecked = extension.is_checked;
+
+    if (!isChecked) {
+      Extensions.update(
+        { is_checked: true },
+        {
+          where: {
+            extension: extensionName,
+          },
+        }
+      );
+    }
+
+    return extension;
+  };
+}
