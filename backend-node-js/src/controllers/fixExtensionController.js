a1421a66ed9c3806a653a11e0af20a9ba387f9c7
sw801733 <sw801733@naver.com>
2024-07-11 22:01:16 +0900

    feat : fixExtensionController 에서 서비스 로직 호출

--- backend-node-js/src/controllers/fixExtensionController.js
+++ backend-node-js/src/controllers/fixExtensionController.js
@@ -1,18 +1,33 @@
+// 서비스 인스턴스 생성
+const fixExtensionService = require('../services/fixExtensionService');
 
+class fixExtensionController {
 
-export class fixExtensionController {
-
-    // 서비스 인스턴스 생성
+    fixExtensionService = new fixExtensionService();
 
     getAllFixExtension = async (req, res, next) => {
         // 모든 고정 확장자 조회
+        const extensions = await this.fixExtensionService.findAllEnabledExtensions();
+
+        console.log("고정 확장자 조회");
+        res.status(200).json({data : extensions});
     }
 
     enableFixExtension = async (req, res, next) => {
         // 고정 확장자 활성화
+        const extension = await this.fixExtensionService.addExtension();
+
+        console.log("고정 확장자 활성화");
+        res.status(200).json({data : extension});
     }
 
     disableFixExtension = async (req, res, next) => {
         // 고정 확장자 비활성화
+        const extension = await this.fixExtensionService.deleteExtension();
+
+        console.log("고정 확장자 비활성화");
+        res.status(200).json({data : extension});
     }
 }
+
+module.exports = fixExtensionController;
